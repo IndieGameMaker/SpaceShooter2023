87b22acdeb8032e2e01243826231b073c3a6a520
Lee JaeHyun <myleje@gmail.com>
2023-07-01 15:18:09 +0900

    .

---
+++ Assets/02_Scripts/Fire.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+    public Transform firePos;
+    public GameObject bulletPrefab;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
