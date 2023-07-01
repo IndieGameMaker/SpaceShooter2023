6bbdefdb91d12a488e27f09997479968a6e73edc
Lee JaeHyun <myleje@gmail.com>
2023-07-01 17:20:06 +0900

    .

--- Assets/02_Scripts/Barrel.cs
+++ Assets/02_Scripts/Barrel.cs
@@ -20,11 +20,11 @@ public class Barrel : MonoBehaviour
 
     void ExpBarrel()
     {
-        GetComponent<Rigidbody>().AddForce(Vector3.up * 20000.0f);
+        GetComponent<Rigidbody>().AddForce(Vector3.up * 30000.0f);
         // Explosion Effect
         GameObject effect = Instantiate(expEffect, transform.position, Quaternion.identity);
         Destroy(effect, 2.5f);
 
-        Destroy(this.gameObject, 3.0f);
+        Destroy(this.gameObject, 1.0f);
     }
 }
