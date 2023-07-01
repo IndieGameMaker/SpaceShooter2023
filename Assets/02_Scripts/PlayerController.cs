4c7181ffbaa49f8194ce1a1b0d9bf398650795bc
Lee JaeHyun <myleje@gmail.com>
2023-07-01 11:35:32 +0900

    /

--- Assets/02_Scripts/PlayerController.cs
+++ Assets/02_Scripts/PlayerController.cs
@@ -18,7 +18,8 @@ public class PlayerController : MonoBehaviour
         float h = Input.GetAxis("Horizontal"); // A, D , Left Arrow, Right Arrow // -1.0f ~ 0.0f ~ +1.0f
         float v = Input.GetAxis("Vertical"); // W, S, Up, Down // -1.0f ~ 0.0f ~ +1.0f
 
-
+        //tr.position += new Vector3(0, 0, 0.1f); // tr.position = tr.position + new Vector3(0, 0, 0.1f);
+        tr.position += Vector3.forward * 0.1f;
 
         Debug.Log("h=" + h);
         Debug.Log("v=" + v);
